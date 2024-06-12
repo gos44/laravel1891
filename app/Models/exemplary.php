4cdf7036781e71d92de259542d041be859a01dd6
gustavoandressanchezceron@gmail.com <gustavoandressanchezceron@gmail.com>
2024-06-12 13:04:23 -0500

    ss

--- app/Models/exemplary.php
+++ app/Models/exemplary.php
@@ -2,18 +2,18 @@
 
     namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\Factories\HasFactory;
+    use Illuminate\Database\Eloquent\Model;
 
-class exemplary extends Model
-{
-    use HasFactory;
-    public function book(){
-        return $this->belongsTo('App\Models\book');
-    }
-    //Relacion muchos a muchos
-        public function ExemplaryUser(){
-          return $this->belongsToMany('App\Models\exemplary_user');
-      }
+    class exemplary extends Model
+    {
+        use HasFactory;
+        public function book(){
+            return $this->belongsTo('App\Models\book');
+        }
+        //Relacion muchos a muchos
+            public function ExemplaryUser(){
+            return $this->belongsToMany('App\Models\exemplary_user');
+        }
 
     }
